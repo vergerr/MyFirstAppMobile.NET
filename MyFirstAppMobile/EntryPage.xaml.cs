c72bd7d215b9bfba8e6f3477e380b02a9ad63aa3
Richard VERGER <vergerr@gmail.com>
2026-01-12 16:05:39 +0100

    Modification/Delete + application différent fix

--- MyFirstAppMobile/EntryPage.xaml.cs
+++ MyFirstAppMobile/EntryPage.xaml.cs
@@ -1,13 +1,26 @@
 using MyFirstAppMobile.Models;
 using MyFirstAppMobile.ViewModels;
+using System.Threading.Tasks;
 
 namespace MyFirstAppMobile;
 
-public partial class EntryPage : ContentPage
+public partial class EntryPage : ContentPage, IQueryAttributable
 {
-	public EntryPage(EntryFormViewModel vm)
+	private readonly EntryFormViewModel _viewModel;
+
+    public EntryPage(EntryFormViewModel vm)
 	{
 		InitializeComponent();
+        _viewModel = vm;
 		BindingContext = vm;
 	}
+
+    async void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.TryGetValue("EntryId", out var value) && value is Guid id)
+            await _viewModel.LoadForEditAsync(id);
+        else
+            _viewModel.ResetForCreate();
+    }
+
 }
--- MyFirstAppMobile/ViewModels/EntriesViewModel.cs
+++ MyFirstAppMobile/ViewModels/EntriesViewModel.cs
@@ -10,25 +10,44 @@ namespace MyFirstAppMobile.ViewModels
     public partial class EntriesViewModel : ObservableObject
     {
         private readonly IEntriesRepository _repo;
-        public ObservableCollection<FitnessEntry> Entries { get; set; } = new();
+        public ObservableCollection<FitnessEntry> Entries { get; } = new();
 
         public EntriesViewModel(IEntriesRepository repo)
         {
             _repo = repo;
-            // Réception du message venant du formulaire
+            // Handler de réception du message venant du formulaire pour une nouvelel EntryFitness
             WeakReferenceMessenger.Default.Register<NewEntryMessage>(this, async (r, m) =>
             {
-                await repo.AddAsync(m.Value);
+                await _repo.AddAsync(m.Value);
             });
+
+            //Handler de réception pour modification d'un EntryFitness
+            WeakReferenceMessenger.Default.Register<UpdateEntryMessage>(this, async (r, m) =>
+            {
+                await _repo.UpdateAsync(m.Value);
+
+                var current = Entries.FirstOrDefault(x => x.Id == m.Value.Id);
+                if (current is null) return;
+
+                var idx = Entries.IndexOf(current);
+                Entries[idx] = m.Value;
+
+            });
+
+
         }
 
-        private readonly SemaphoreSlim _semaphore = new(1,1);
+        private readonly SemaphoreSlim _semaphore = new(1, 1);
 
-        [ObservableProperty]
         private bool isLoading;
+        public bool CanInteract => !isLoading;
+        void OnIsLoadingChanged()
+        {
+            OnPropertyChanged(nameof(CanInteract));
+        }
 
         [RelayCommand]
-        public async Task Load ()
+        public async Task Load()
         {
             if (!await _semaphore.WaitAsync(0))
                 return;
@@ -57,11 +76,20 @@ namespace MyFirstAppMobile.ViewModels
         }
 
         [RelayCommand]
+        public async Task Edit(Guid id)
+        {
+            await Shell.Current.GoToAsync(nameof(EntryPage), new Dictionary<string, object>
+            {
+                ["EntryId"] = id
+            });
+        }
+
+        [RelayCommand]
         public async Task Delete(Guid id)
         {
             await _repo.DeleteAsync(id);
-            FitnessEntry fe = Entries.First(e => e.Id == id);
-            if (fe == null)
+            var fe = Entries.FirstOrDefault(e => e.Id == id);
+            if (fe is null)
                 return;
             Entries.Remove(fe);
         }
